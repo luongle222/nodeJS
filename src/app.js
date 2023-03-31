99b251f92558c63317fad377e00c696f5f6a12e8
luongle222 <luongldph20822@gmail.com>
2023-03-31 13:15:19 +0700

    up

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import productRouter from "./Router/Products.js";
 import authRouter from "./Router/Auth.js";
 import mongoose from "mongoose";
+import categoryRouter from "./models/category.js";
+import cors from "cors"
 
 
 dotenv.config();
@@ -11,9 +13,11 @@ const app = express();
 
 //đăng kí middleware
 app.use(express.json());
+app.use(cors())
 
-app.use(productRouter);
-app.use(authRouter);
+app.use("/api", productRouter);
+app.use("/api", categoryRouter);
+app.use("/api", authRouter);
 
 mongoose.connect("mongodb://127.0.0.1:27017/we17302");
 
